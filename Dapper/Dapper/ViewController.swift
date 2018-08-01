d5d09d865c00ce279a118f8d1b9be8fdaf0a786c
Zach Dingels <Zach@Zachs-MacBook-Air.local>
2018-08-01 14:01:25 -0700

    Installed AWS Podfile

---
+++ Dapper/Dapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dapper
+//
+//  Created by Zach Dingels on 8/1/18.
+//  Copyright © 2018 Zach Dingels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

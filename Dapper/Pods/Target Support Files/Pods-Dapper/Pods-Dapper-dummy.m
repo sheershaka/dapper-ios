4d73ec7b9e4332da672bc415599bb57ee6836768
Zach Dingels <>
2018-08-09 20:30:55 -0700

    added Info.plist to .gitignore

--- Dapper/Pods/Target Support Files/Pods-Dapper/Pods-Dapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dapper : NSObject
-@end
-@implementation PodsDummy_Pods_Dapper
-@end
--- Dapper/Pods/Target Support Files/Pods-DapperTests/Pods-DapperTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DapperTests : NSObject
-@end
-@implementation PodsDummy_Pods_DapperTests
-@end

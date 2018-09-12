d6ca98b63222b64be3e63c5bbf78236d3a12f202
Sheershak Agarwal <sa1998@uw.edu>
2018-09-12 21:56:11 +0530

    braintree integration

--- Dapper/Dapper/Model/DAPPaymentManager.swift
+++ Dapper/Dapper/Model/DAPPaymentManager.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import BraintreeDropIn
+import Braintree
 
 // Class: ParentClass is the syntax for subclassing so in this case we are making a class called
 // DAPPaymentManager and its a subclass of NSObject. NSObject is basically the default class that everything inherits from
@@ -33,4 +35,51 @@ class DAPPaymentManager: NSObject {
         
     }
     
+    func fetchClientToken() {
+        // TODO: Switch this URL to your own authenticated API
+        let clientTokenURL = NSURL(string: "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_96PLwtoLp/.well-known/jwks.json")!
+        let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
+        clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
+            // TODO: Handle errors
+            let clientToken = String(data: data!, encoding: String.Encoding.utf8)
+            print(clientToken!)
+            self.showDropIn(clientTokenOrTokenizationKey: clientToken!)
+            // As an example, you may wish to present Drop-in at this point.
+            // Continue to the next section to learn more...
+            }.resume()
+    }
+    
+    func showDropIn(clientTokenOrTokenizationKey: String) {
+        let request =  BTDropInRequest()
+        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
+        { (controller, result, error) in
+            if (error != nil) {
+                print("ERROR")
+            } else if (result?.isCancelled == true) {
+                print("CANCELLED")
+            } else if let result = result {
+                // Use the BTDropInResult properties to update your UI
+                //result.paymentOptionType
+                //result.paymentMethod
+                //result.paymentIcon
+                //result.paymentDescription
+            }
+            controller.dismiss(animated: true, completion: nil)
+        }
+        //self.present(dropIn!, animated: true, completion: nil)
+    }
+    /*
+    func postNonceToServer(paymentMethodNonce: String) {
+        // Update URL with your server
+        let paymentURL = URL(string: "https://your-server.example.com/payment-methods")!
+        var request = URLRequest(url: paymentURL)
+        request.httpBody = "payment_method_nonce=\(paymentMethodNonce)".data(using: String.Encoding.utf8)
+        request.httpMethod = "POST"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+            // TODO: Handle success or failure
+            }.resume()
+    }*/
 }
